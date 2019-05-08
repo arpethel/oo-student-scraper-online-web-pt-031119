57a7f32349aa6596cfb6d1fd8f310a5071b10a05
Ryan Pethel <arpethel@gmail.com>
2019-05-08 22:00:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,43 +22,39 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     student_doc = doc.css(".main-wrapper")
-    # binding.pry
-    # student_doc.each do |student|
     student = student_doc[0]
-      profile_hash = {}
-      # binding.pry
-      student.css("a").each do |hash|
-        hash.each do |k, v|
-          if k == "href" && v.include?("twitter")
-            profile_hash[:twitter] = v
-          elsif k == "href" && v.include?("linkedin")
-            profile_hash[:linkedin] = v
-          elsif k == "href" && v.include?("github")
-            profile_hash[:github] = v
-          elsif k == "href" && v.include?("http")
-            profile_hash[:blog] = v
-          end
-        end
-      end
+    profile_hash = {}
 
-      student.css("div").each do |hash|
-        hash.each do |k, v|
-          if v == "profile-quote"
-            profile_hash[:profile_quote] = hash.text
-          end
+    student.css("a").each do |hash|
+      hash.each do |k, v|
+        if k == "href" && v.include?("twitter")
+          profile_hash[:twitter] = v
+        elsif k == "href" && v.include?("linkedin")
+          profile_hash[:linkedin] = v
+        elsif k == "href" && v.include?("github")
+          profile_hash[:github] = v
+        elsif k == "href" && v.include?("http")
+          profile_hash[:blog] = v
         end
       end
-
-      student.css("div").each do |hash|
-        hash.each do |k, v|
-          if v.include?("bio-content")
-            profile_hash[:bio] = hash.css("p").text.strip
-          end
-        end
-      end
-      profile_hash
     end
-    # profile_hash
-  # end
+
+    student.css("div").each do |hash|
+      hash.each do |k, v|
+        if v == "profile-quote"
+          profile_hash[:profile_quote] = hash.text
+        end
+      end
+    end
+
+    student.css("div").each do |hash|
+      hash.each do |k, v|
+        if v.include?("bio-content")
+          profile_hash[:bio] = hash.css("p").text.strip
+        end
+      end
+    end
+    profile_hash
+  end
 
 end
