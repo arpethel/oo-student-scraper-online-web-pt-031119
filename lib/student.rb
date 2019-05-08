763686759a29fcf2d3bc123bc8af2e9052f0255a
Ryan Pethel <arpethel@gmail.com>
2019-05-08 22:11:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,20 +5,15 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    # binding.pry
     @name = student_hash[:name]
     @location = student_hash[:location]
     @@all << self
   end
 
   def self.create_from_collection(students_array)
-    # binding.pry
     students_array.each do |student_hash|
-      # binding.pry
       self.new(student_hash)
-      # Scraper.scrape_index_page(student_hash)
     end
-    # binding.pry
   end
 
   def add_student_attributes(attributes_hash)
